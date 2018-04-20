2058e5ab616f13b34dc9b36691a4e4b738c361e4
Martsunovska <chinatsu079@gmail.com>
2018-04-20 21:26:01 +0300

    lab-4, 5

--- module-03/lab-04/app/labController.js
+++ module-03/lab-04/app/labController.js
@@ -1,10 +1,11 @@
 app.controller("labController", [
-    "$scope", "$timeout", "$q", "$http",
+    "$scope", "$timeout", "$q", "$http", "gitHub",
 
-    function($scope, $timeout, $q, $http) {
+    function($scope, $timeout, $q, $http, gitHub) {
         $scope.model = {
             number: 0,
             result: "Ready",
+            fil: ""
         };
         $scope.checkOddNumber = checkOddNumber;
         $scope.getRepos = getRepos;
@@ -36,23 +37,14 @@ app.controller("labController", [
             return !isNaN(input) && input % 2 == 1;
         }
 
-        function getRepos() {
-            $http.get("https://api.github.com/orgs/angular/repos")
-                .then(function(response) {
-                    $scope.model.repos = response.data;
-                }, function(response) {
-                    $scope.model.repos = "Error: " + response.data.message;
-                });
+        function getRepos(fil) {
+            $scope.model.repos = gitHub.getAll({ org: fil });
         }
 
+
         function loadDetail(name) {
-            var url = 'https://api.github.com/repos/angular/' + name;
-            $http.get(url)
-                .then(function(response) {
-                    $scope.model.detail = response.data;
-                }, function(response) {
-                    $scope.model.detail = { error: true, message: 'Error: ' + response.data.message };
-                });
+            $scope.model.detail = null;
+            $scope.model.detail = gitHub.getDetail({ id: name });
         }
 
     }
